5ac0defebae095d97332f838645dd829b7d3bd9c
Camilo Cuadrado Bertollo <cuadradocamilo1@gmail>
2025-08-31 20:23:39 -0300

    Terminada Agenda, Pasan todos los Tests

--- Taller2/src/main/java/aed/Agenda.java
+++ Taller2/src/main/java/aed/Agenda.java
@@ -6,6 +6,7 @@ public class Agenda {
 
     public Agenda(Fecha fechaActual) {
         this.dia_de_hoy = fechaActual;
+        this.recordatorios = new ArregloRedimensionableDeRecordatorios();
     }
 
     public void agregarRecordatorio(Recordatorio recordatorio) {
@@ -17,8 +18,8 @@ public class Agenda {
         String mensaje_final = this.dia_de_hoy + "\n=====\n";
         for (int indice = 0; indice<this.recordatorios.longitud(); indice++) {
             Recordatorio recordatorio_en_indice = this.recordatorios.obtener(indice);
-            if (recordatorio_en_indice.fecha() == this.dia_de_hoy) {
-                mensaje_final = mensaje_final+recordatorio_en_indice.toString();
+            if (recordatorio_en_indice.fecha().equals(this.dia_de_hoy)) {
+                mensaje_final = mensaje_final+recordatorio_en_indice.toString()+"\n";
             }
         }
         return mensaje_final;
