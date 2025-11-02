a6d0c99b2e44ae2cb02070a4d58d8010f069576b
Camilo Cuadrado Bertollo <cuadradocamilo1@gmail>
2025-11-02 18:58:48 -0300

    Terminado Arbol, creo

--- Taller 5 - Estructuras enlazadas/src/main/java/aed/ABB.java
+++ Taller 5 - Estructuras enlazadas/src/main/java/aed/ABB.java
@@ -2,7 +2,6 @@ package aed;
 
 public class ABB<T extends Comparable<T>> {
     private Nodo root;
-    private int tamano;
 
     private class Nodo {
         T valor;
@@ -37,6 +36,68 @@ public class ABB<T extends Comparable<T>> {
             }
             return Buscador;
         }
+
+        private Nodo minimoNodal(){
+            Nodo Buscador = this;
+            while (Buscador.I != null){
+                Buscador = Buscador.I;
+            }
+            return Buscador;
+        }
+
+        private int Cantidad_de_hijos(){
+            int res = 0;
+            if (this.D != null){
+                res = res+1;
+            }
+            if (this.I != null){
+                res = res+1;
+            }
+            return res;
+        }
+
+        private void BorradorNodal(){
+            int caso = this.Cantidad_de_hijos();
+            if (caso == 0){
+                if (this.A != null){
+                    if (this.A.valor.compareTo(this.valor) > 0){
+                        this.A.I = null;
+                    }
+                    else{
+                        this.A.D = null;
+                    }
+                    this.A = null;
+                }
+                this.valor = null;
+            }
+            if (caso == 1){
+                Nodo Hijo = this;
+                if (this.I != null){
+                    Hijo = this.I;
+                }
+                else{
+                    Hijo = this.D;
+                }
+                if (this.A != null){
+                    Hijo.A = this.A;
+                    if (this.A.valor.compareTo(this.valor) > 0){
+                        this.A.I = Hijo;
+                    }
+                    else{
+                        this.A.D = Hijo;
+                    }
+                }
+                else{
+                    root = Hijo;
+                    Hijo.A = null;
+                }
+            }
+            if (caso == 2){
+                Nodo minimo = this.D.minimoNodal();
+                this.valor = minimo.valor;
+                minimo.BorradorNodal();
+            }
+        }
     }
 
     public class HandleABB {
@@ -49,51 +110,111 @@ public class ABB<T extends Comparable<T>> {
         public T valor(){
             return this.nodoReferenciado.valor;
         }
+
+        public void Borrar(){
+            this.nodoReferenciado.BorradorNodal();
+        }
     }
 
     public ABB() {
-        this.tamano = 0;
+        this.root = null;
     }
 
     public T minimo(){
-        throw new UnsupportedOperationException("No implementado aún");
+        return this.root.minimoNodal().valor;
     }
 
     public HandleABB insertar(T elem){
-        Nodo Buscador = this.root.BuscadorNodal(elem);
-        if (Buscador.valor.compareTo(elem) != 0){
-            Nodo insertado = new Nodo(elem);
-            insertado.D = Buscador;
-            if (Buscador.valor.compareTo(elem) > 0){
-                Buscador.I = insertado;
-            }
-            else{
-                Buscador.D = insertado;
-            }
-            Buscador = insertado;
+        Nodo Buscador = this.root;
+        if ((this.root == null) || (this.root.valor == null)){
+            Buscador = new Nodo(elem);
+            this.root = Buscador;
         }
-        return new HandleABB(Buscador);
+        else{
+            Buscador = Buscador.BuscadorNodal(elem);
+            if (Buscador.valor.compareTo(elem) != 0){
+                Nodo insertado = new Nodo(elem);
+                insertado.A = Buscador;
+                if (Buscador.valor.compareTo(elem) > 0){
+                    Buscador.I = insertado;
+                }
+                else{
+                    Buscador.D = insertado;
+                }
+                Buscador = insertado;
+            }
+        }
+        HandleABB Palanca = new HandleABB(Buscador);
+        return Palanca;
     }
 
     public boolean pertenece(T elem){
-        throw new UnsupportedOperationException("No implementado aún");
+        Nodo Buscador = this.root.BuscadorNodal(elem);
+        boolean res = (Buscador.valor.compareTo(elem) == 0);
+        return res;
     }
 
     public void eliminar(T elem){
-        throw new UnsupportedOperationException("No implementado aún");
+        Nodo Buscador = this.root.BuscadorNodal(elem);
+        if (Buscador.valor.compareTo(elem) == 0){
+            Buscador.BorradorNodal();
+        }
     }
 
     @Override
     public String toString(){
-        throw new UnsupportedOperationException("No implementado aún");
+        String texto = "{";
+        ABB_Iterador iterador = this.iterador();
+        if (iterador.haySiguiente()){
+            texto = texto+iterador.siguiente();
+        }
+        while (iterador.haySiguiente()){
+            texto = texto+","+iterador.siguiente();
+        }
+        texto = texto+"}";
+        return texto;
     }
 
     public class ABB_Iterador {
-        /* ¡COMPLETAR! */
+        private Nodo _actual;
+
+        private ABB_Iterador(){
+            if (root == null){
+                _actual = null;
+            }
+            else{
+                _actual = root.minimoNodal();
+            }
+        }
+
+        public boolean haySiguiente() {
+            return (this._actual != null);
+        }
+    
+        public T siguiente() {
+            T res = this._actual.valor;
+            if (this._actual.D != null){
+                this._actual = this._actual.D.minimoNodal();
+            }
+            else{
+                Nodo Buscador = this._actual.A;
+                boolean Encontrado = false;
+                while ((!Encontrado) && (Buscador != null)){
+                    if (Buscador.valor.compareTo(this._actual.valor) > 0){
+                        Encontrado = true;
+                    }
+                    else{
+                        Buscador = Buscador.A;
+                    }
+                }
+                this._actual = Buscador;
+            }
+            return res;
+        }
     }
 
     public ABB_Iterador iterador() {
-        throw new UnsupportedOperationException("No implementado aún");
+        return new ABB_Iterador();
     }
 
 }
