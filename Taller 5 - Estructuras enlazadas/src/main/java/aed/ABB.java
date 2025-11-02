8ab08cb9516cea4d407068fe4da859ff3fec0585
Camilo Cuadrado Bertollo <cuadradocamilo1@gmail>
2025-11-02 17:12:39 -0300

    Me tendría que haber matado a los 16

--- Taller 5 - Estructuras enlazadas/src/main/java/aed/ABB.java
+++ Taller 5 - Estructuras enlazadas/src/main/java/aed/ABB.java
@@ -2,21 +2,57 @@ package aed;
 
 public class ABB<T extends Comparable<T>> {
     private Nodo root;
-    private int cantidad;
+    private int tamano;
 
     private class Nodo {
         T valor;
         Nodo I; //Izquierda
         Nodo D; //Derecha
         Nodo A; //Arriba
+
+        private Nodo(T elem){
+            this.valor = elem;
+        }
+
+        private Nodo BuscadorNodal(T elem){
+            Nodo Buscador = this;
+            boolean noEsta = false;
+            while ((!noEsta) && (Buscador.valor.compareTo(elem) != 0)){
+                if (Buscador.valor.compareTo(elem) > 0){
+                    if (Buscador.I != null){
+                        Buscador = Buscador.I;
+                    }
+                    else{
+                        noEsta = true;
+                    }
+                }
+                else{
+                    if (Buscador.D != null){
+                        Buscador = Buscador.D;
+                    }
+                    else{
+                        noEsta = true;
+                    }
+                }
+            }
+            return Buscador;
+        }
     }
 
     public class HandleABB {
-        private 
+        Nodo nodoReferenciado;
+
+        private HandleABB(Nodo referencia){
+            this.nodoReferenciado = referencia;
+        }
+
+        public T valor(){
+            return this.nodoReferenciado.valor;
+        }
     }
 
     public ABB() {
-        throw new UnsupportedOperationException("No implementado aún");
+        this.tamano = 0;
     }
 
     public T minimo(){
@@ -24,7 +60,19 @@ public class ABB<T extends Comparable<T>> {
     }
 
     public HandleABB insertar(T elem){
-        throw new UnsupportedOperationException("No implementado aún");
+        Nodo Buscador = this.root.BuscadorNodal(elem);
+        if (Buscador.valor.compareTo(elem) != 0){
+            Nodo insertado = new Nodo(elem);
+            insertado.D = Buscador;
+            if (Buscador.valor.compareTo(elem) > 0){
+                Buscador.I = insertado;
+            }
+            else{
+                Buscador.D = insertado;
+            }
+            Buscador = insertado;
+        }
+        return new HandleABB(Buscador);
     }
 
     public boolean pertenece(T elem){
